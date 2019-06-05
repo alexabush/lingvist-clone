d58e46189f2ba2bdb47c51787f00e0517ff52ae1
Alex Bush <alexabush7@gmail.com>
2019-06-05 17:18:58 -0500

    basic card component with working toggle, animation needs more work

--- src/comp/Card.js
+++ src/comp/Card.js
@@ -1,14 +1,38 @@
-import R from 'react'
-import T from 'prop-types'
-
+import R from 'react';
+import T from 'prop-types';
+import { Animate } from 'react-simple-animate';
 export default class SFCard extends R.PureComponent {
   static propTypes = {
     children: T.any,
-  }
+  };
+  state = { isHide: false };
+
+  toggleShow = () => {
+    this.setState(state => {
+      return {
+        isHide: !state.isHide,
+      };
+    });
+  };
 
   render() {
-    return (
-      <div>This is a card</div>
-    )
+    let { children } = this.props;
+    let { isHide } = this.state;
+
+    let childComponents = React.Children.map(children, (child, idx) => {
+      if (children.length === 3 && idx === 0) {
+        return <div onClick={this.toggleShow}>{child}</div>;
+      } else if (children.length === 3 && idx === 1) {
+        return (
+          <Animate play={isHide} start={{ height: '20px' }} end={{ height: '0px' }} complete={{ display: 'none' }}>
+            {child}
+          </Animate>
+        );
+      } else {
+        return child;
+      }
+    });
+
+    return <div>{childComponents}</div>;
   }
 }
