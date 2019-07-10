b9c6bcb7a8b326bc9876ef7ebd720d59d9931abd
Alexander Bush <alexabush7@gmail.com>
2019-07-10 15:45:24 -0500

    cleaned up index page to prep for merging in master and lingvist

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,11 @@
-import ExerciseCard from '../src/comp/ExerciseCard';
-import SFChiclet from '../src/comp/SFChiclet';
-import RepsScroller from '../src/comp/RepsScroller';
 import React from 'react';
-class ExampleChiclets extends React.PureComponent {
-  state = {
-    isSelected1: true,
-    isSelected2: false
-  };
-
-  handleClick1 = () => {
-    this.setState(state => {
-      return { isSelected1: !state.isSelected1 };
-    });
-  };
-  handleClick2 = () => {
-    this.setState(state => {
-      return { isSelected2: !state.isSelected2 };
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <SFChiclet
-          onClick={this.handleClick1}
-          selected={this.state.isSelected1}
-        >
-          10
-        </SFChiclet>
-        <SFChiclet
-          onClick={this.handleClick2}
-          selected={this.state.isSelected2}
-        >
-          9
-        </SFChiclet>
-        <SFChiclet onClick={this.handleClick} outline>
-          8
-        </SFChiclet>
-      </div>
-    );
-  }
-}
+import ExerciseCard from '../src/comp/ExerciseCard';
 
 export default function DisplayAll() {
   return (
-    <div>
-      <div style={{ margin: '30px' }}>
-        <h2>Example SFScroller</h2>
-        <RepsScroller />
-      </div>
-      <div style={{ margin: '30px' }}>
-        <h2>Example Chiclets</h2>
-        <ExampleChiclets />
-      </div>
-      <div style={{ margin: '30px' }}>
-        <h2>SFCard</h2>
-        <ExerciseCard />
-      </div>
+    <div style={{ margin: '30px' }}>
+      <h2>SFCard</h2>
+      <ExerciseCard />
     </div>
   );
 }
